3204fefd6a1c988b4ac9c61768ca21c78c5718ec
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-02-20 13:56:26 -0800

    blackjack score method

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,10 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# FACE_CARDS = ["King", "Queen", "Jack"]
 
 def blackjack_score(hand)
-
+  score = hand.reduce(:+)
 end
+
+blackjack_score(VALID_CARDS)
